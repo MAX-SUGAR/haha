32dec0bd7bb55017c356e51cd42c334be47fa06b
MAX-SUGAR <958241514@qq.com>
2016-05-30 17:05:30 +0800

    galaxywind-phone-index

---
+++ galaxywind-phone-index.js
@@ -0,0 +1,35 @@
+
+
+/*nav fold*/
+$("#fold").on("click",function(){
+    if($("#fold ul").is(":hidden")){
+        $("#fold ul").show("1500");
+    }else{
+        $("#fold ul").hide("1500");
+    }
+})
+
+
+/*company profile*/
+function change(i){
+
+   $("#company_info .information").eq(i).on("tap",function(){
+
+     $("#company_info ul").eq(i).is(":hidden") ? $("#company_info ul").eq(i).show("1500") && $("#company_info h3").eq(i).html("×"): $("#company_info ul").eq(i).hide("1500") && $("#company_info h3").eq(i).html("+");
+
+   })
+}
+
+var list=$("#company_info ul")
+for(var i=0;i<list.length;i++){
+    change(i);
+}
+
+
+/*wechat*/
+$("#popup").on("tap",function(){
+   alert("请关注银河风云官方微信号galaxy-wind");
+})
+
+
+
